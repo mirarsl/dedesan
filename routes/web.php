4fbb120b9261918605aef35679aa07e2caf28d3b
Batuhan Arslan <81113714+mirarsl@users.noreply.github.com>
2024-08-26 22:58:24 +0300

    Görsel sıralama

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MyVoyagerController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 /*
@@ -15,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
+
+    Route::post('orderFiles/{table}/{field}/{id}', [MyVoyagerController::class,'orderFiles'])->name('voyager.orderFiles');
+    Route::post('orderImages/{table}/{field}/{id}', [MyVoyagerController::class,'orderImages'])->name('voyager.orderImages');
 });
 Route::post('search', [PageController::class, 'search'])->name('search');
 Route::post('store', [PageController::class, 'store'])->name('store');
