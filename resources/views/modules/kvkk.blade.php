34b14a99b9c8310120e195bdcfa89b9e0c9792ef
Batuhan Arslan <81113714+mirarsl@users.noreply.github.com>
2025-03-05 22:52:39 +0300

    Create kvkk.blade.php

---
+++ resources/views/modules/kvkk.blade.php
@@ -0,0 +1,13 @@
+<div class="about-area-2 space-top overflow-hidden">
+    <div class="container">
+      <div class="row gx-60 align-items-start flex-row-reverse">
+        <div class="col-xl-12">
+          <div class="title-area mb-25">
+            <span class="sub-title">{{$module->top}}</span>
+            <h2 class="sec-title">{{$module->title}}</h2>
+            <p class="sec-text">{!! $module->text !!}</p>
+          </div>             
+        </div>
+      </div>
+    </div>
+  </div>
