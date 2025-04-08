225e4f3d91a4dd8a3925412782d0d9e2b5f3ea13
Batuhan Arslan <81113714+mirarsl@users.noreply.github.com>
2025-04-08 22:44:06 +0300

    düzenlemeler

--- resources/views/modules/projects.blade.php
+++ resources/views/modules/projects.blade.php
@@ -1,4 +1,7 @@
 <div class="portfolio-area-1 space-bottom">
+	<div class="container text-center">
+		{!! $module->text !!}
+	</div>
 	<div class="title-area text-center">
 		<span class="sub-title">{{$module->top}}</span>
 		<h2 class="sec-title">{{$module->title}}</h2>
@@ -14,7 +17,7 @@
 					<div class="portfolio-card-details">
 						<div class="media-left">
 							<span class="portfolio-card-subtitle">{{$item->getType()}}</span>
-							<h3 class="portfolio-card-title"><a href="{{route('project',$item->slug)}}">{{$item->title}}</a></h3>
+							<div class="portfolio-card-title"><a href="{{route('project',$item->slug)}}">{{$item->title}}</a></div>
 							<p class="portofolio-card-text">{{$item->short_text}}</p>
 						</div>  
 					</div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.master')
 @section('content')
-<h1 class="w-0 opacity-0"><?=setting('site.title')?></h1>
+<h1 class="w-0 opacity-0">Kocaeli İnşaat Firması</h1>
 @foreach ($Page->modules as $module)
 @if (View::exists('modules.' . $module->slug))
 @include('modules.' . $module->slug, ['module' => $module])
